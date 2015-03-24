183a8b1023e958d6a86e593d12bca3bc8eb83b7d
Nasc <brase.yns@gmail.com>
2015-03-24 14:06:44 +0000

    Mise à jour et ajout de fichiers de test

--- index.php
+++ index.php
@@ -4,21 +4,15 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 </head>
 
-<div id="gmenu">
-    <p id="button" onclick="toggle('moreinfo','button');" onmouseover="hoverBox(event,'divtoshow');" onmouseout="hoverBoxKill('divtoshow')">+ d'info</p>
-</div>
-<div id="moreinfo">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius augue vel ipsum rutrum, eget posuere erat malesuada. Vivamus scelerisque tempor interdum. Aliquam rhoncus, turpis vitae dignissim dignissim, purus nibh ultrices lacus, a efficitur metus nisl vestibulum lectus. Fusce consequat quam sed lorem luctus, ac fermentum mi sollicitudin. Curabitur pharetra velit nec aliquam bibendum. Sed vestibulum est ut eros molestie dignissim. Praesent et sapien diam. Etiam lacinia egestas velit, vel varius ipsum placerat ut. Fusce sodales dapibus tellus. </p>
-</div>
-<div id="divtoshow">
-    Ceci est un chouette bouton.
+<h1> Bienvenue à moi sur ma page de tests !</h1>
+<div id="wrap">
+		<p>Voilà toutes les petites choses que je testes quand j'ai du temps libre.</p>
+		<a href="chaton.php">Super chat avec moi même</a>
+		<a href="blog.php">Super blog nul</a>
+		<a href="img.php"> Comment ? Des images générée avec php ?</a>
 </div>
 
-<span onmouseover="hoverBox(event,'JDan');" onmouseout="hoverBoxKill('JDan');"> Cliquez pour plus d'informations : </span>
 
-<a href="http://empireceleste.xooit.org/t54-JackDaniels-le-ma-tre-de-la-rage.htm#p422"><img src="http://img.xooimage.com/files110/6/e/d/ibol-499503b.png" class="hollow" alt="Ibo" onmouseover="hoverBox(event,'JDan');" onmouseout="hoverBoxKill('JDan');"></a><a href="http://empireceleste.xooit.org/t55-Zatno-l-artiste.htm#p423" ><img src="http://img.xooimage.com/files110/a/6/a/ger-4995051.png" class="hollow" alt="Zatno" onmouseover="hoverBox(event,'Zatno');" onmouseout="hoverBoxKill('Zatno');"></a>
-<div id="JDan" class="hoverBox">JackDaniel</div>
-<div id="Zatno" class="hoverBox">Zatno</div>
 
 <!--Code : 1 4 6 26 42 3 11
 
