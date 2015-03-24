f47cd953aa6fb5be1ee896f77af626e9a6e800ac
Nasc <brase.yns@gmail.com>
2015-03-24 15:10:31 +0000

    Derniers tests faits

---
+++ img.php
@@ -0,0 +1,10 @@
+<?php
+	header("Content-type:image/png");
+	$img=imagecreate(200,200);
+
+	$belorange = imagecolorallocate($img, 255, 128, 0);
+	$blancdezappy = imagecolorallocate($img, 255, 255, 254);
+	imagestring($img, 5, 90, 100, "Salut !", $blancdezappy);
+
+	imagepng($img);
+?>
--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <head>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8" />
     <script type="text/javascript" src="toggle.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 </head>
 
 <h1> Bienvenue à moi sur ma page de tests !</h1>
 <div id="wrap">
-		<p>Voilà toutes les petites choses que je testes quand j'ai du temps libre.</p>
-		<a href="chaton.php">Super chat avec moi même</a>
-		<a href="blog.php">Super blog nul</a>
-		<a href="img.php"> Comment ? Des images générée avec php ?</a>
+	<p>Voilà toutes les petites choses que je testes quand j'ai du temps libre.</p>
+	<ul>
+		<li><a href="chaton.php">Super chat avec moi même</a></li>
+		<li><a href="blog.php">Super blog nul</a></li>
+		<li><a href="img.php"> Comment ? Des images générée avec php ?</a></li>
+	</ul>
 </div>
 
 
